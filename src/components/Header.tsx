82ef6f1e05794085289ffe45dd586c7003355fb9
6714120005-crypto <6714120005@mut.ac.th>
2025-12-24 11:23:11 +0700

    Add files via upload

---
+++ src/components/Header.tsx
@@ -0,0 +1,13 @@
+import './Header.css';  // Import CSS file
+
+// Function Component อย่างง่าย
+function Header() {
+    return (
+        <header className="header">
+            <h1>🎮 Pokemon App</h1>
+            <p>Explore the world of Pokemon!</p>
+        </header>
+    );
+}
+
+export default Header;
